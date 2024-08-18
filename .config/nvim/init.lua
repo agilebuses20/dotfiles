94b9f221295b5118a72d97c07b5ba7fba0e45e86
Alan <123632636+agilebuses20@users.noreply.github.com>
2024-08-18 13:16:22 +0100

    Fixed nvim config files

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,16 +1,3 @@
-<<<<<<< HEAD
--- Include files
-require('config.options')
-require('config.lazy')
-
-require('lualine').setup {
-    options = {
-        icons_enabled = false
-    }
-}
-
-require('nvim-tree').setup()
-=======
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -19,4 +6,3 @@ require('config.options')
 require('config.lazy')
 
 vim.cmd.colorscheme "catppuccin"
->>>>>>> af03316 (Replaced nvim config files)
--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,5 +1,2 @@
 vim.opt.number = true
-<<<<<<< HEAD
-=======
 vim.opt.termguicolors = true
->>>>>>> af03316 (Replaced nvim config files)
